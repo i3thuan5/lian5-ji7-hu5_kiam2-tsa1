fa08beb0cc997a0bd9d02da657ca999b31c8a575
a8568730 <a8568730@hotmail.com>
2018-02-08 16:59:50 +0800

    fix: test_虛詞原則試驗，正確文字為原本連寫的詞的正確書寫

--- tests/test_虛詞原則試驗.py
+++ tests/test_虛詞原則試驗.py
@@ -6,18 +6,16 @@ from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
     # 虛詞原則一：介詞與前後詞分寫
     ('伊佇頭前', 'I tī thâu-tsîng', None),
     ('伊佇頭前', 'I-tī thâu-tsîng', [('E虛詞（一）', 2, 'I tī')]),
-    ('伊佇頭前', 'I--tī thâu-tsîng', [('E虛詞（一）', 2, 'I tī')]),
     ('伊佇頭前', 'I tī-thâu-tsîng', [('E虛詞（一）', 2, 'tī thâu-tsîng')]),
-    ('伊佇頭前', 'I tī--thâu-tsîng', [('E虛詞（一）', 2, 'tī thâu-tsîng')]),
+    ('伊佇頭前', 'I-tī-thâu-tsîng', [('E虛詞（一）', 2, 'i tī thâu-tsîng')]),
     ('講予你知', 'kóng hōo lí tsai', None),
-    ('講予你知', 'kóng hōo--lí tsai', [('E虛詞（一）', 2, 'kóng hōo lí tsai')]),
+    ('講予你知', 'kóng hōo-lí tsai', [('E虛詞（一）', 2, 'kóng hōo lí tsai')]),
     # 虛詞原則二：連詞語前後詞分寫 
     # 佮、閣、抑、愈
     ('頭家佮辛勞', 'Thâu-ke kah sin-lô', None),
-    ('頭家佮辛勞', 'Thâu-ke-kah sin-lô', [('E虛詞（二）', 2, 'Thâu-ke kah sin-lô')]),
-    ('頭家佮辛勞', 'Thâu-ke kah-sin-lô', [('E虛詞（二）', 2, 'Thâu-ke kah sin-lô')]),
-    ('頭家佮辛勞', 'Thâu-ke-kah-sin-lô', [('E虛詞（二）', 2, 'Thâu-ke kah sin-lô')]),
-    ('頭家佮辛勞', 'Thâu-ke--kah sin-lô', [('E虛詞（二）', 2, 'Thâu-ke kah sin-lô')]),
+    ('頭家佮辛勞', 'Thâu-ke-kah sin-lô', [('E虛詞（二）', 3, 'Thâu-ke kah')]),
+    ('頭家佮辛勞', 'Thâu-ke kah-sin-lô', [('E虛詞（二）', 3, 'kah sin-lô')]),
+    ('頭家佮辛勞', 'Thâu-ke-kah-sin-lô', [('E虛詞（二）', 3, 'Thâu-ke kah sin-lô')]),
     ('愈來愈乖', 'Jú lâi-jú kuai', [('E虛詞（二）', 3, 'Jú lâi jú kuai')]),
     ('愈來愈乖', 'Jú-lâi jú kuai', [('E虛詞（二）', 1, 'Jú lâi jú kuai')]),
     ('愈來愈乖', 'Jú-lâi-jú kuai', [('E虛詞（二）', 1, 'Jú lâi jú kuai'), ('E虛詞（一）', 3, 'Jú lâi jú kuai')]),
