e8f8639a932037a8c9a6532fd8f2779719d08e06
a8568730 <a8568730@hotmail.com>
2018-02-09 17:38:05 +0800

    fix: test_二_連詞試驗，函式補上test_

--- tests/虛詞原則/test_二_連詞試驗.py
+++ tests/虛詞原則/test_二_連詞試驗.py
@@ -11,48 +11,48 @@ class 單元試驗(TestCase):
     # 虛詞原則二：連詞語前後詞分寫
     # 佮、閣、抑、愈
 
-    def 連詞_佮_正確(self):
+    def test_連詞_佮_正確(self):
         self.漢字 = '頭家佮辛勞'
         self.臺羅 = 'Thâu-ke kah sin-lô'
         self.預期 = []
 
-    def 連詞_佮_前面不連寫(self):
+    def test_連詞_佮_前面不連寫(self):
         self.漢字 = '頭家佮辛勞'
         self.臺羅 = 'Thâu-ke-kah sin-lô'
         self.預期 = [('E虛詞（二）', 3, '前', 'Thâu-ke kah')]
 
-    def 連詞_佮_後面不連寫(self):
+    def test_連詞_佮_後面不連寫(self):
         self.漢字 = '頭家佮辛勞'
         self.臺羅 = 'Thâu-ke kah-sin-lô'
         self.預期 = [('E虛詞（二）', 3, '後', 'kah sin-lô')]
 
-    def 連詞_佮_前後皆不連寫(self):
+    def test_連詞_佮_前後皆不連寫(self):
         self.漢字 = '頭家佮辛勞'
         self.臺羅 = 'Thâu-ke-kah-sin-lô'
         self.預期 = [('E虛詞（二）', 3, '前', 'Thâu-ke kah'),
                    ('E虛詞（二）', 3, '後', 'kah sin-lô')]
 
-    def 連詞_第一個愈_後面不連寫(self):
+    def test_連詞_第一個愈_後面不連寫(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú-lâi jú kuai'
         self.預期 = [('E虛詞（二）', 1, '後', 'Jú lâi')]
 
-    def 連詞_第二個愈_後面不連寫(self):
+    def test_連詞_第二個愈_後面不連寫(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi jú-kuai'
         self.預期 = [('E虛詞（二）', 3, '後', 'Jú lâi jú kuai')]
     
-    def 連詞_第二個愈_前面不連寫(self):
+    def test_連詞_第二個愈_前面不連寫(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi-jú kuai'
         self.預期 = [('E虛詞（二）', 3, '前', 'Jú lâi jú kuai')]
 
-    def 連詞_愈愈_不夾別詞(self):
+    def test_連詞_愈愈_不夾別詞(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú-lâi-jú kuai'
         self.預期 = [('E虛詞（二）', 1, '後', 'Jú lâi'), ('E虛詞（二）', 3, '前', 'lâi jú')]
 
-    def 連詞_愈愈_不當詞嵌(self):
+    def test_連詞_愈愈_不當詞嵌(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi-jú-kuai'
         self.預期 = [('E虛詞（二）', 3, '前', 'lâi jú'), ('E虛詞（二）', 3, '後', 'jú kuai')]
