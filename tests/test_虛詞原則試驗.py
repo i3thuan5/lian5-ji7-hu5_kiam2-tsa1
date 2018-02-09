12689f1aed960a9b5b903567eafebfede6d81d32
a8568730 <a8568730@hotmail.com>
2018-02-09 15:51:32 +0800

    fix: test_虛詞原則試驗，修改assert方式

--- tests/test_虛詞原則試驗.py
+++ tests/test_虛詞原則試驗.py
@@ -7,7 +7,7 @@ from src.連字符檢查.虛詞原則.虛詞 import 是否符合原則
 class 單元試驗(TestCase):
     def tearDown(self):
         句物件 = 拆文分析器.對齊句物件(self.漢字, self.臺羅)
-        assert 是否符合原則(句物件) == self.預期
+        self.assertEqual(是否符合原則(句物件), self.預期)
     
     # 虛詞原則一：介詞與前後詞分寫
     def test_虛詞原則一_佇_正確(self):
@@ -30,6 +30,11 @@ class 單元試驗(TestCase):
         self.臺羅 = 'I-tī-thâu-tsîng'
         self.預期 = [('E虛詞（一）', 2, '前', 'i tī'), ('E虛詞（一）', 2, '後', 'tī thâu-tsîng')]
     
+    def test_虛詞原則一_佇_長詞(self):
+        self.漢字 = '頭家佇頭前'
+        self.臺羅 = 'Thâu-ke-tī-thâu-tsîng'
+        self.預期 = [('E虛詞（一）', 3, '前', 'Thâu-ke tī'), ('E虛詞（一）', 3, '後', 'tī thâu-tsîng')]
+        
     def test_虛詞原則一_予_正確(self):
         self.漢字 = '講予你知'
         self.臺羅 = 'kóng hōo lí tsai'
