70b51d0c6f6402fd30f45aad7d7403cb8fc46699
a8568730 <a8568730@hotmail.com>
2018-02-12 14:36:10 +0800

    fix: test_二_連詞試驗，測試多個佮

--- tests/虛詞原則/test_二_連詞試驗.py
+++ tests/虛詞原則/test_二_連詞試驗.py
@@ -31,6 +31,17 @@ class 單元試驗(TestCase):
         self.臺羅 = 'Thâu-ke-kah-sin-lô'
         self.預期 = [('E虛詞（二）', 3, '前', 'Thâu-ke kah'),
                    ('E虛詞（二）', 3, '後', 'kah sin-lô')]
+    
+    def test_連詞_多個佮(self):
+        self.漢字 = '我佮伊佮爸爸佮阿母'
+        self.臺羅 = 'Guá-kah-i-kah-pah-pah-kah-a-bú'
+        self.預期 = [
+            ('E虛詞（二）', 2, '前', 'Guá kah'),
+            ('E虛詞（二）', 2, '後', 'kah i'),
+            ('E虛詞（二）', 4, '前', 'i kah'),
+            ('E虛詞（二）', 4, '後', 'kah pah-pah'),
+            ('E虛詞（二）', 7, '前', 'pah-pah kah'),
+            ('E虛詞（二）', 7, '後', 'kah a-bú'),]
 
     def test_連詞_第一個愈_後面不連寫(self):
         self.漢字 = '愈來愈乖'
@@ -41,7 +52,7 @@ class 單元試驗(TestCase):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi jú-kuai'
         self.預期 = [('E虛詞（二）', 3, '後', 'jú kuai')]
-    
+
     def test_連詞_第二個愈_前面不連寫(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi-jú kuai'
@@ -51,18 +62,27 @@ class 單元試驗(TestCase):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú-lâi-jú kuai'
         self.預期 = [('E虛詞（二）', 1, '後', 'Jú lâi'), ('E虛詞（二）', 3, '前', 'lâi jú')]
-    
+
     def test_連詞_愈愈_不夾詞_長詞(self):
         self.漢字 = '愈大漢愈乖'
         self.臺羅 = 'Jú-tuā-hàn-jú kuai'
-        self.預期 = [('E虛詞（二）', 1, '後', 'Jú tuā-hàn'), ('E虛詞（二）', 3, '前', 'tuā-hàn jú')]
+        self.預期 = [('E虛詞（二）', 1, '後', 'Jú tuā-hàn'),
+                   ('E虛詞（二）', 3, '前', 'tuā-hàn jú')]
 
     def test_連詞_愈愈_不當詞嵌(self):
         self.漢字 = '愈來愈乖'
         self.臺羅 = 'Jú lâi-jú-kuai'
         self.預期 = [('E虛詞（二）', 3, '前', 'lâi jú'), ('E虛詞（二）', 3, '後', 'jú kuai')]
-    
+
     def test_連詞_愈愈_不當詞嵌_長詞(self):
         self.漢字 = '愈來愈大漢'
         self.臺羅 = 'Jú lâi-jú-tuā-hàn'
-        self.預期 = [('E虛詞（二）', 3, '前', 'lâi jú'), ('E虛詞（二）', 3, '後', 'jú tuā-hàn')]
+        self.預期 = [('E虛詞（二）', 3, '前', 'lâi jú'),
+                   ('E虛詞（二）', 3, '後', 'jú tuā-hàn')]
+
+    def test_連詞_愈愈_不串在一起(self):
+        self.漢字 = '愈來愈乖'
+        self.臺羅 = 'Jú-lâi-jú-kuai'
+        self.預期 = [('E虛詞（二）', 1, '後', 'Jú lâi'),
+                   ('E虛詞（二）', 3, '前', 'lâi jú'), 
+                   ('E虛詞（二）', 3, '後', 'jú kuai')]
