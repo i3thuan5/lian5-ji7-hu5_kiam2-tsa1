f3b846e7b1991fed8f7320d5d4dba1351c5f60c4
a8568730 <a8568730@hotmail.com>
2018-02-12 17:47:57 +0800

    fix: test_介詞/連詞試驗，預期格式改為(錯誤代碼, 位置, 前後)

--- tests/虛詞原則/test_一_介詞試驗.py
+++ tests/虛詞原則/test_一_介詞試驗.py
@@ -7,44 +7,44 @@ class 單元試驗(TestCase):
     def tearDown(self):
         句物件 = 拆文分析器.對齊句物件(self.漢字, self.臺羅)
         self.assertEqual(是否符合介詞原則(句物件), self.預期)
-    
+
     # 虛詞原則一：介詞與前後詞分寫
     def test_虛詞原則一_佇_正確(self):
         self.漢字 = '伊佇頭前'
         self.臺羅 = 'I tī thâu-tsîng'
         self.預期 = []
-    
+
     def test_虛詞原則一_佇_前面不連寫(self):
         self.漢字 = '伊佇頭前'
         self.臺羅 = 'I-tī thâu-tsîng'
-        self.預期 = [('E虛詞（一）', 2, '前', 'I tī')]
-    
+        self.預期 = [('E虛詞（一）', 2, '前')]
+
     def test_虛詞原則一_佇_後面不連寫(self):
         self.漢字 = '伊佇頭前'
         self.臺羅 = 'I tī-thâu-tsîng'
-        self.預期 = [('E虛詞（一）', 2, '後', 'tī thâu-tsîng')]
-    
+        self.預期 = [('E虛詞（一）', 2, '後')]
+
     def test_虛詞原則一_佇_前後皆不連寫(self):
         self.漢字 = '伊佇頭前'
         self.臺羅 = 'I-tī-thâu-tsîng'
-        self.預期 = [('E虛詞（一）', 2, '前', 'I tī'), ('E虛詞（一）', 2, '後', 'tī thâu-tsîng')]
-    
+        self.預期 = [('E虛詞（一）', 2, '前'), ('E虛詞（一）', 2, '後')]
+
     def test_虛詞原則一_佇_長詞(self):
         self.漢字 = '頭家佇頭前'
         self.臺羅 = 'Thâu-ke-tī-thâu-tsîng'
-        self.預期 = [('E虛詞（一）', 3, '前', 'Thâu-ke tī'), ('E虛詞（一）', 3, '後', 'tī thâu-tsîng')]
-    
+        self.預期 = [('E虛詞（一）', 3, '前'), ('E虛詞（一）', 3, '後')]
+
     def test_虛詞原則一_佇_數字調_正確(self):
         self.漢字 = '伊佇頭前'
         self.臺羅 = 'I1 ti7 thau5-tsing5'
         self.預期 = []
-        
+
     def test_虛詞原則一_予_正確(self):
         self.漢字 = '講予你知'
         self.臺羅 = 'kóng hōo lí tsai'
         self.預期 = []
-    
+
     def test_虛詞原則一_予_後面不連寫(self):
         self.漢字 = '講予你知'
         self.臺羅 = 'kóng hōo-lí tsai'
-        self.預期 = [('E虛詞（一）', 2, '後', 'hōo lí')]
+        self.預期 = [('E虛詞（一）', 2, '後')]
