fe8975054bc915069d9e257016440ce0f92a9439
a8568730 <a8568730@hotmail.com>
2018-02-14 11:14:35 +0800

    fix: test_時貌標誌試驗，修改測驗

--- tests/動詞原則/test_時貌標誌試驗.py
+++ tests/動詞原則/test_時貌標誌試驗.py
@@ -11,62 +11,68 @@ class 單元試驗(TestCase):
     # 動詞原則二：動詞和後接時貌標誌連寫
     # 咧、著、過
 
-    def test_咧_正確(self):
+    def test_徛咧講_正確(self):
         self.漢字 = '徛咧講'
         self.臺羅 = 'Khiā-leh kóng'
         self.預期 = []
 
-    def test_咧_錯誤_前面必須連寫(self):
+    def test_徛咧講_錯誤_前面必須連寫(self):
         self.漢字 = '徛咧講'
         self.臺羅 = 'Khiā leh kóng'
         self.預期 = [('E動詞（二）', 2, '前')]
 
-    def test_咧_錯誤_後面動詞不連寫(self):
+    def test_徛咧講_錯誤_後面動詞不連寫(self):
         self.漢字 = '徛咧講'
         self.臺羅 = 'Khiā-leh-kóng'
         self.預期 = [('E動詞（二）', 2, '後')]
 
-    def test_咧_錯誤_後面有動詞就不用輕聲調(self):
+    def test_徛咧講_錯誤_前連寫後分寫(self):
         self.漢字 = '徛咧講'
-        self.臺羅 = 'Khiā--leh-kóng'
-        self.預期 = [('E動詞（二）', 2, '前')]
-
-    def test_過_錯誤_後面有詞就不用輕聲調(self):
-        self.漢字 = '看過書'
-        self.臺羅 = 'Khuànn--kuè tsu'
-        # 正確 Khuànn-kuè tsu
-        self.預期 = [('E輕聲符', 2, '前')]
-
-    def test_咧_錯誤_前連寫後分寫(self):
-        self.漢字 = '徛咧講'
-        self.臺羅 = 'Khiā-leh-kóng'
+        self.臺羅 = 'Khiā leh-kóng'
         self.預期 = [('E動詞（二）', 2, '前'), ('E動詞（二）', 2, '後')]
 
-    def test_咧輕聲調_正確(self):
+    def test_徛咧講_錯誤_後面有詞就不用輕聲調(self):
+        self.漢字 = '徛咧講'
+        self.臺羅 = 'Khiā--leh kóng'
+        self.預期 = [('E動詞（二）', 2, '前')]
+
+    def test_看過書_錯誤_不用輕聲調且後面分寫(self):
+        self.漢字 = '看過書'
+        self.臺羅 = 'Khuànn--kuè-tsu'
+        # 正確 Khuànn-kuè tsu
+        self.預期 = [('E動詞（二）', 2, '前'), ('E動詞（二）', 2, '後')]
+
+    def test_徛咧_正確(self):
         self.漢字 = '徛咧'
         self.臺羅 = 'Khiā--leh'
         self.預期 = []
 
-    def test_咧輕聲調_錯誤_應是輕聲符(self):
+    def test_徛咧_錯誤_應是輕聲符(self):
         self.漢字 = '徛咧'
         self.臺羅 = 'Khiā-leh'
         # 正確 'Khiā--leh'
-        self.預期 = [('E輕聲符', 2, '前')]
+        self.預期 = [('E動詞（二）', 2, '前')]
 
-    def test_咧輕聲調_錯誤_句尾嘆詞不影響咧(self):
+    def test_徛咧_錯誤_動詞和咧不斷開(self):
+        self.漢字 = '徛咧矣'
+        self.臺羅 = 'Khiā leh--ah'
+        # 正確 'Khiā--leh'
+        self.預期 = [('E動詞（二）', 2, '前')]
+
+    def test_徛咧_錯誤_句尾嘆詞不影響輕聲符(self):
         self.漢字 = '徛咧矣'
         self.臺羅 = 'Khiā-leh--ah'
         # 正確 'Khiā--leh'
-        self.預期 = [('E輕聲符', 2, '前')]
-
-    def test_咧輕聲調_錯誤_語尾助詞不影響咧(self):
+        self.預期 = [('E動詞（二）', 2, '前')]
+        
+    def test_徛咧_錯誤_語尾助詞不影響輕聲符(self):
         self.漢字 = '欲徛咧無？'
         self.臺羅 = 'Beh khiā-leh--bô?'
         # 正確 'Khiā--leh'
-        self.預期 = [('E輕聲符', 3, '前')]
+        self.預期 = [('E動詞（二）', 3, '前')]
 
-    def test_著和結構助詞_正確(self):
+    def test_我看著的_錯誤_結構助詞不影響輕聲符(self):
         self.漢字 = '我看著的'
-        self.臺羅 = 'Guá khuànn--tio̍h--ê'
-        # 正確 'Khiā--leh'
-        self.預期 = [('E輕聲符', 3, '前')]
+        self.臺羅 = 'Guá khuànn-tio̍h--ê'
+        # 正確 Guá khuànn--tio̍h--ê
+        self.預期 = [('E動詞（二）', 3, '前')]
