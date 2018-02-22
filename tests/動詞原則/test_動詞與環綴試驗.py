d887e9cfe9428c75f5e5b604ff70091a8fae610e
pigu <ihcaoe@gmail.com>
2018-02-22 16:49:42 +0800

    feat: 動詞，+是否符合環綴原則()

--- tests/動詞原則/test_動詞與環綴試驗.py
+++ tests/動詞原則/test_動詞與環綴試驗.py
@@ -1,5 +1,6 @@
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from unittest.case import TestCase
+from src.連字符檢查.動詞原則.動詞 import 是否符合環綴原則
 
 
 class 單元試驗(TestCase):
@@ -11,21 +12,21 @@ class 單元試驗(TestCase):
     # 會...得，袂...得
 
     def test_會記得_正確(self):
-        self.漢字 = '會記得'
-        self.臺羅 = 'ē-kì-tit'
+        self.漢字 = '我會記得你'
+        self.臺羅 = 'Guá ē-kì-tit lí'
         self.預期 = []
     
-    def test_會記得_錯誤(self):
-        self.漢字 = '會記得'
-        self.臺羅 = 'ē kì-tit'
-        self.預期 = [('E動詞（一）', 1, '前')]
+    def test_會記得_錯誤_首字應連寫(self):
+        self.漢字 = '姑娘會記得你'
+        self.臺羅 = 'Koo-niû ē kì-tit lí'
+        self.預期 = [('E動詞（一）', 3, '後')]
 
-    def test_會記得_錯誤_前後應連寫(self):
-        self.漢字 = '會記得'
-        self.臺羅 = 'ē kì tit'
-        self.預期 = [('E動詞（一）', 1, '前'), ('E動詞（一）', 1, '後')]
+    def test_會記得_錯誤_尾字應連寫(self):
+        self.漢字 = '姑娘會記得你'
+        self.臺羅 = 'Koo-niû ē-kì tit lí'
+        self.預期 = [('E動詞（一）', 4, '前')]
     
     def test_袂記得_正確(self):
-        self.漢字 = '袂記得'
-        self.臺羅 = 'bē-kì--tit'
+        self.漢字 = '我袂記得你'
+        self.臺羅 = 'Guá bē-kì--tit lí'
         self.預期 = []
