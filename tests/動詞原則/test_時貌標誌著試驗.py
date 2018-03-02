3bf945e6c85ea898299cba683706d6e56d4a3c00
a8568730 <a8568730@hotmail.com>
2018-03-02 18:13:05 +0800

    fix: 時貌標誌著試驗，修改註解、處理著不是時貌是動詞的情況。

--- tests/動詞原則/test_時貌標誌著試驗.py
+++ tests/動詞原則/test_時貌標誌著試驗.py
@@ -9,9 +9,11 @@ class 單元試驗(TestCase):
         self.assertEqual(是否符合著原則(句物件), self.預期)
 
     # 動詞原則二：動詞和後接時貌標誌連寫
-    # 咧、著、過
-    # 徛-咧 講
-    # 咧 看 冊  (動詞前面的時貌標誌分寫）
+    
+    # 動詞和時貌著連寫 我看著 => 我看-著
+    # 著是動詞，跳過不處理 著你
+    # 著是動詞一部份，跳過不處理 我著-病
+    
     def test_我看著錢_正確(self):
         self.漢字 = '我看著錢'
         self.臺羅 = 'Guá khuànn-tio̍h tsînn'
@@ -21,7 +23,7 @@ class 單元試驗(TestCase):
         self.漢字 = '我看著錢'
         self.臺羅 = 'Guá khuànn tio̍h tsînn'
         self.預期 = [('E動詞（二）', 3, '前'), ]
-    
+
     def test_我看著錢_錯誤_不是結尾應正常變調(self):
         self.漢字 = '我看著錢'
         self.臺羅 = 'Guá khuànn--tio̍h tsînn'
@@ -31,22 +33,22 @@ class 單元試驗(TestCase):
         self.漢字 = '我看著錢'
         self.臺羅 = 'Guá khuànn-tio̍h-tsînn'
         self.預期 = [('E動詞（二）', 3, '後'), ]
-        
+
     def test_我看著的錢_正確(self):
         self.漢字 = '我看著的錢'
         self.臺羅 = 'Guá khuànn--tio̍h ê tsînn'
         self.預期 = []
-    
+
     def test_我看著的錢_錯誤_結構助詞之前應輕聲調(self):
         self.漢字 = '我看著的錢'
         self.臺羅 = 'Guá khuànn-tio̍h ê tsînn'
         self.預期 = [('E動詞（二）', 3, '前'), ]
-    
-    def test_我看著的_正確(self):
-        self.漢字 = '我看著的。'
-        self.臺羅 = 'Guá khuànn--tio̍h--ê.'
+
+    def test_我看著矣_正確(self):
+        self.漢字 = '我看著矣。'
+        self.臺羅 = 'Guá khuànn--tio̍h--ah.'
         self.預期 = []
-    
+
     def test_我有看著_正確(self):
         self.漢字 = '我有看著'
         self.臺羅 = 'Guá ū khuànn--tio̍h'
@@ -55,4 +57,11 @@ class 單元試驗(TestCase):
     def test_我有看著_錯誤_結尾應輕聲調(self):
         self.漢字 = '我有看著。'
         self.臺羅 = 'Guá ū khuànn-tio̍h.'
-        self.預期 = [('E動詞（二）', 3, '前'),]
+        self.預期 = [('E動詞（二）', 3, '前'), ]
+
+    def test_著是動詞_我著病(self):
+        self.漢字 = '我著病。'
+        self.臺羅 = 'Guá tio̍h-pīnn.'
+        self.預期 = []
+    
+    
