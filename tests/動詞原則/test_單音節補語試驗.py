a41c1eeff15890f9ef00f75be847008bc5117b36
a8568730 <a8568730@hotmail.com>
2018-03-06 18:28:02 +0800

    fix: 動詞，是否符合單音節補語，單動+單補應連寫。

--- tests/動詞原則/test_單音節補語試驗.py
+++ tests/動詞原則/test_單音節補語試驗.py
@@ -1,6 +1,6 @@
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from unittest.case import TestCase, skip
-from src.連字符檢查.動詞原則.動詞 import 是否符合著原則
+from unittest.case import TestCase
+from src.連字符檢查.動詞原則.動詞 import 符合單音節補語原則
 
 
 class 單元試驗(TestCase):
@@ -9,23 +9,35 @@ class 單元試驗(TestCase):
         self.assertEqual(符合單音節補語原則(句物件), self.預期,
                          (self.漢字, self.臺羅))
 
-    # 動詞原則三：單音節動詞或形容詞，和後接補語連寫    
+    # 動詞原則三：單音節動詞或形容詞，和後接補語連寫。
+    # 其餘情況分寫。
+
     def test_講煞_正確(self):
         self.漢字 = '我講煞'
         self.臺羅 = 'Guá kóng-suah'
         self.預期 = []
-        
-    def test_講煞_錯誤_要連寫(self):
+
+    def test_講煞_要連寫(self):
         self.漢字 = '我講煞'
         self.臺羅 = 'Guá kóng suah'
         self.預期 = [('E動詞（三）', 3, '前')]
+    
+    def test_我煞_前面不是動詞(self):
+        self.漢字 = '我煞'
+        self.臺羅 = 'Guá-suah'
+        self.預期 = [('E動詞（三）', 2, '前')]
         
-    def test_整理好_錯誤(self):
+    def test_整理好_要分寫(self):
         self.漢字 = '整理好'
-        self.臺羅 = 'Tsíng-lí hó'
+        self.臺羅 = 'Tsíng-lí-hó'
         self.預期 = [('E動詞（三）', 3, '前')]
 
-    def test_排好勢(self):
+    def test_排好勢_要分寫(self):
         self.漢字 = '排好勢'
         self.臺羅 = 'Pâi-hó-sè'
         self.預期 = [('E動詞（三）', 2, '前')]
+
+    def test_整理好勢_要分寫(self):
+        self.漢字 = '整理好勢'
+        self.臺羅 = 'Tsíng-lí-hó-sè'
+        self.預期 = [('E動詞（三）', 3, '前')]
