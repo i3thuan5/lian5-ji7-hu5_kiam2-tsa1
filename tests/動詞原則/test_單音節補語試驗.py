9127f504aa6db04dfa237e9b791f2b356669a7e0
a8568730 <a8568730@hotmail.com>
2018-03-06 14:58:17 +0800

    feat: 動詞，+test_單音節補語試驗

---
+++ tests/動詞原則/test_單音節補語試驗.py
@@ -0,0 +1,31 @@
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from unittest.case import TestCase, skip
+from src.連字符檢查.動詞原則.動詞 import 是否符合著原則
+
+
+class 單元試驗(TestCase):
+    def tearDown(self):
+        句物件 = 拆文分析器.對齊句物件(self.漢字, self.臺羅)
+        self.assertEqual(符合單音節補語原則(句物件), self.預期,
+                         (self.漢字, self.臺羅))
+
+    # 動詞原則三：單音節動詞或形容詞，和後接補語連寫    
+    def test_講煞_正確(self):
+        self.漢字 = '我講煞'
+        self.臺羅 = 'Guá kóng-suah'
+        self.預期 = []
+        
+    def test_講煞_錯誤_要連寫(self):
+        self.漢字 = '我講煞'
+        self.臺羅 = 'Guá kóng suah'
+        self.預期 = [('E動詞（三）', 3, '前')]
+        
+    def test_整理好_錯誤(self):
+        self.漢字 = '整理好'
+        self.臺羅 = 'Tsíng-lí hó'
+        self.預期 = [('E動詞（三）', 3, '前')]
+
+    def test_排好勢(self):
+        self.漢字 = '排好勢'
+        self.臺羅 = 'Pâi-hó-sè'
+        self.預期 = [('E動詞（三）', 2, '前')]
