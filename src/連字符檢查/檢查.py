41f1d6c7aeb2a20fa82c282c960ddb7aee8e0cea
a8568730 <a8568730@hotmail.com>
2018-03-13 15:58:23 +0800

    feat: 連字符檢查，呼叫名詞原則的函式

--- src/連字符檢查/檢查.py
+++ src/連字符檢查/檢查.py
@@ -1,15 +1,14 @@
-from src.連字符檢查.名詞原則.單音節詞綴 import 單音節詞綴原則
+from src.連字符檢查.名詞原則 import 單音節詞綴, 方位詞, 序列詞頭
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 
 class 連字符檢查物件:
     句物件 = None
-    
+
     def __init__(self, 漢字, 臺羅):
         self.句物件 = 拆文分析器.對齊句物件(漢字, 臺羅)
-        
-    def 檢查(self):
-        self.檢查名詞()
 
-    def 檢查名詞(self):
-        單音節詞綴原則(self.句物件)
+    def 檢查(self):
+        單音節詞綴.單音節詞綴原則(self.句物件)
+        方位詞.方位詞原則(self.句物件)
+        序列詞頭.序列詞頭原則(self.句物件)
--- tests/test_連字符檢查.py
+++ tests/test_連字符檢查.py
@@ -1,34 +1,27 @@
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from unittest.case import TestCase
 from src.連字符檢查.檢查 import 連字符檢查物件
-# from unittest.mock import MagicMock
 from unittest import mock
 from unittest.mock import patch
 
 
 class mock試驗(TestCase):
+    def setUp(self):
+        self.一物件 = 連字符檢查物件('漢字', 'Hàn-jī')
+        
     @patch('src.連字符檢查.名詞原則.單音節詞綴.單音節詞綴原則')
     def test用著單音節詞綴原則(self, 單音節詞綴原則):
-        一物件 = 連字符檢查物件('漢字', 'Hàn-jī')
-        一物件.檢查()
+        self.一物件.檢查()
         單音節詞綴原則.assert_called_once()
-
-# class 整合試驗(TestCase):
-#     def tearDown(self):
-#         句物件 = 拆文分析器.對齊句物件(self.漢字, self.臺羅)
-#         self.assertEqual(檢查連字符(句物件), self.預期)
-# 
-#     def test名詞原則(self):
-#         self.漢字 = '阿公初一蹛山頂啦，'
-#         self.臺羅 = 'A kong tshe it tuà suann tíng--lah,'
-#         self.預期 = [('E名詞（一）', 2, '前'), ('E名詞（二）', 4, '前'),  ('E名詞（二）', 7, '前')]
-# 
-#     def test動詞原則(self):
-#         self.漢字 = ''
-#         self.臺羅 = ''
-#         self.預期 = []
-# 
-#     def test虛詞原則(self):
-#         self.漢字 = ''
-#         self.臺羅 = ''
-#         self.預期 = []
+        
+    @patch('src.連字符檢查.名詞原則.方位詞.方位詞原則')
+    def test用著方位詞原則(self, 方位詞原則):
+        self.一物件.檢查()
+        方位詞原則.assert_called_once()
+    
+    @patch('src.連字符檢查.名詞原則.序列詞頭.序列詞頭原則')
+    def test用著序列詞頭原則(self, 序列詞頭原則):
+        self.一物件.檢查()
+        序列詞頭原則.assert_called_once()
+    
+    
