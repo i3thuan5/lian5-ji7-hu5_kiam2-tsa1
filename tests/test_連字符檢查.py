ff2835c789b372a3e57ac6883f68184b8048b2ea
a8568730 <a8568730@hotmail.com>
2018-03-13 16:24:55 +0800

    feat: 連字符檢查，+動詞和虛詞原則。

--- tests/test_連字符檢查.py
+++ tests/test_連字符檢查.py
@@ -8,20 +8,69 @@ from unittest.mock import patch
 class mock試驗(TestCase):
     def setUp(self):
         self.一物件 = 連字符檢查物件('漢字', 'Hàn-jī')
-        
+
+    # 名詞原則
+
     @patch('src.連字符檢查.名詞原則.單音節詞綴.單音節詞綴原則')
     def test用著單音節詞綴原則(self, 單音節詞綴原則):
         self.一物件.檢查()
         單音節詞綴原則.assert_called_once()
-        
+
     @patch('src.連字符檢查.名詞原則.方位詞.方位詞原則')
     def test用著方位詞原則(self, 方位詞原則):
         self.一物件.檢查()
         方位詞原則.assert_called_once()
-    
+
     @patch('src.連字符檢查.名詞原則.序列詞頭.序列詞頭原則')
     def test用著序列詞頭原則(self, 序列詞頭原則):
         self.一物件.檢查()
         序列詞頭原則.assert_called_once()
-    
-    
+
+    # 動詞原則
+
+    @patch('src.連字符檢查.動詞原則.動詞.是否符合環綴原則')
+    def test用著環綴原則(self, 是否符合環綴原則):
+        self.一物件.檢查()
+        是否符合環綴原則.assert_called_once()
+
+    @patch('src.連字符檢查.動詞原則.動詞.是否符合咧原則')
+    def test用著咧原則(self, 是否符合咧原則):
+        self.一物件.檢查()
+        是否符合咧原則.assert_called_once()
+
+    @patch('src.連字符檢查.動詞原則.動詞.是否符合著原則')
+    def test用著著原則(self, 是否符合著原則):
+        self.一物件.檢查()
+        是否符合著原則.assert_called_once()
+
+    @patch('src.連字符檢查.動詞原則.動詞.符合單音節補語原則')
+    def test用著單音節補語原則(self, 符合單音節補語原則):
+        self.一物件.檢查()
+        符合單音節補語原則.assert_called_once()
+
+    # 虛詞原則
+
+    @patch('src.連字符檢查.虛詞原則.虛詞.是否符合介詞原則')
+    def test用著介詞原則(self, 是否符合介詞原則):
+        self.一物件.檢查()
+        是否符合介詞原則.assert_called_once()
+
+    @patch('src.連字符檢查.虛詞原則.虛詞.是否符合連詞原則')
+    def test用著連詞原則(self, 是否符合連詞原則):
+        self.一物件.檢查()
+        是否符合連詞原則.assert_called_once()
+
+    @patch('src.連字符檢查.虛詞原則.虛詞.是否符合結構助詞原則')
+    def test用著結構助詞原則(self, 是否符合結構助詞原則):
+        self.一物件.檢查()
+        是否符合結構助詞原則.assert_called_once()
+
+    @patch('src.連字符檢查.虛詞原則.虛詞.是否符合嘆詞原則')
+    def test用著嘆詞原則(self, 是否符合嘆詞原則):
+        self.一物件.檢查()
+        是否符合嘆詞原則.assert_called_once()
+
+    @patch('src.連字符檢查.虛詞原則.虛詞.是否符合語尾助詞原則')
+    def test用著語尾助詞原則(self, 是否符合語尾助詞原則):
+        self.一物件.檢查()
+        是否符合語尾助詞原則.assert_called_once()
