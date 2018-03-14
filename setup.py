9ce39bc203ac14d85aaf3145c7cf66aae08d4538
a8568730 <a8568730@hotmail.com>
2018-03-14 11:56:38 +0800

    feat: setup.py，更新下載網址版本為0.1-alpha

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lian5-ji7-hu5_kiam2-tsa1',
+  packages = ['lian5-ji7-hu5_kiam2-tsa1'], # this must be the same as the name above
+  version = '0.1',
+  description = '臺羅連字符檢查',
+  author = '意傳',
+  author_email = 'a8568730@gmail.com',
+  url = 'https://github.com/i3thuan5/lian5-ji7-hu5_kiam2-tsa1', # use the URL to the github repo
+  download_url = 'https://github.com/i3thuan5/lian5-ji7-hu5_kiam2-tsa1/archive/0.1-alpha.tar.gz', # I'll explain this in a second
+  keywords = ['台灣閩南語', '書寫', '羅馬字', '檢查', '連字符'], # arbitrary keywords
+  classifiers = [],
+)
