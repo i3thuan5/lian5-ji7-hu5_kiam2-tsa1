e52b152d7559aad0412c2a93bc355b6ed2149e26
a8568730 <a8568730@hotmail.com>
2018-03-16 10:51:46 +0800

    fix: 語尾助詞移除和後面是否連寫的判斷。（無-閒是合法詞）

--- tests/虛詞原則/test_語尾助詞試驗.py
+++ tests/虛詞原則/test_語尾助詞試驗.py
@@ -1,5 +1,4 @@
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from src.連字符檢查.虛詞原則.虛詞 import 是否符合嘆詞原則
 from unittest.case import TestCase, skip
 from src.連字符檢查.虛詞原則.虛詞 import 是否符合語尾助詞原則
 
@@ -12,18 +11,29 @@ class 單元試驗(TestCase):
     # 虛詞原則五：語尾助詞
     # 否、未、無、啥
    
-    def test_語尾助詞_喔_正確(self):
+    def test_語尾助詞無_正確(self):
         self.漢字 = '有錢無'
         self.臺羅 = 'Ū tsînn bô'
         self.預期 = []
     
     @skip('拆文分析器猶閣無法度處理好輕聲調符號')
-    def test_語尾助詞_喔_正確2(self):
+    def test_語尾助詞無_正確2(self):
         self.漢字 = '有錢無'
         self.臺羅 = 'Ū tsînn--bô'
         self.預期 = []
     
-    def test_語尾助詞_喔_前面不連寫(self):
+    def test_語尾助詞無_前面不連寫(self):
         self.漢字 = '有錢無'
         self.臺羅 = 'Ū tsînn-bô'
         self.預期 = [('E虛詞（五）', 3, '前')]
+    
+    def test_多個語尾助詞_皆前面不連寫(self):
+        self.漢字 = '有錢啊乎'
+        self.臺羅 = 'Ū tsînn ah-honnh'
+        self.預期 = [('E虛詞（五）', 4, '前')]
+    
+    def test_逐家攏足無閒_正確(self):
+        self.漢字 = '逐家攏足無閒，'
+        self.臺羅 = 'ta̍k-ke lóng tsiok bô-îng,'
+        self.預期 = []
+        
