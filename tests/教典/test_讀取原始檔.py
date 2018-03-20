9ce739ea9a376acf362b1448108dea23fd914549
a8568730 <a8568730@hotmail.com>
2018-03-20 15:44:23 +0800

    fix: 教典，復原讀取原始檔

---
+++ tests/教典/test_讀取原始檔.py
@@ -0,0 +1,14 @@
+from unittest.case import TestCase
+from src.連字符檢查.教典.讀取原始檔 import 讀取原始檔
+
+
+class 單元試驗(TestCase):
+        #
+        # ('佮意', 'kah-ì', ('動',))
+        # => ['佮-意｜kah4-i3']
+        #
+    def setUp(self):
+        self.教典 = 讀取原始檔()
+
+    def test_原本的詞轉成詞分詞(self):
+        self.assertIn('佮-意｜kah4-i3', self.教典)
