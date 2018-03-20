2d3f6d1ab3fb3999fc99b77a52e1d6ce585bbdcb
a8568730 <a8568730@hotmail.com>
2018-03-20 15:50:50 +0800

    fix: 教典，是教典詞組()，判斷是否在教典陣列裡

--- src/連字符檢查/教典/是教典詞組.py
+++ src/連字符檢查/教典/是教典詞組.py
@@ -1,4 +1,6 @@
+from src.連字符檢查.教典.讀取原始檔 import 讀取原始檔
 
 
 def 是教典詞組(詞分詞):
-    return None
+    教典 = 讀取原始檔()
+    return 詞分詞 in 教典
--- src/連字符檢查/教典/讀取原始檔.py
+++ src/連字符檢查/教典/讀取原始檔.py
@@ -1,6 +1,5 @@
 from os.path import join, dirname
 import ast
-from 臺灣言語工具.基本物件.詞 import 詞
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
@@ -9,7 +8,7 @@ def 讀取原始檔():
     with open(join(dirname(dirname(dirname(dirname(__file__)))), 'moedict-twblg-kiat4-ko2')) as f:
         for line in f:
             # ('佮意', 'kah-ì', ('動',))
-            # => {'佮-意｜kah4-i3': ['佮｜kah4', '意｜i3', ]}
+            # => ['佮-意｜kah4-i3']
             逐逝 = ast.literal_eval(line)
             字分詞陣列 = []
             try:
