d34fabb2aafe6fd6cef71099a7ee112aaf7f3faa
a8568730 <a8568730@hotmail.com>
2018-03-20 14:51:18 +0800

    feat: 讀取原始檔，test_讀取原始檔

---
+++ tests/教典/test_讀取原始檔.py
@@ -0,0 +1,18 @@
+from unittest.case import TestCase
+from src.連字符檢查.教典.讀取原始檔 import 讀取原始檔
+
+
+class 單元試驗(TestCase):
+        #
+        # ('佮意', 'kah-ì', ('動',))
+        # => {'佮-意｜kah4-i3': ['佮｜kah4', '意｜i3', ]}
+        #
+    def setUp(self):
+        self.教典 = 讀取原始檔()
+
+    def test_原本的詞轉成詞分詞(self):
+        self.assertEqual('佮-意｜kah4-i3' in self.教典, True)
+
+    def test_有輸出字分詞陣列(self):
+        self.assertEqual(
+            self.教典['佮-意｜kah4-i3'], ['佮｜kah4', '意｜i3', ])
