40ca917b322c35394245b4f40b5e78b3bead95cc
a8568730 <a8568730@hotmail.com>
2018-03-21 16:42:26 +0800

    fix: setup.py，改package_data的目標資料夾名

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = find_packages(exclude=["test*", "tests"]),
   install_requires=['tai5-uan5_gian5-gi2_kang1-ku7'],
   package_data={
-        '': ['moedict-twblg-kiat4-ko2',],
+        'moedict': ['moedict-twblg-kiat4-ko2',],
     },
 
   author = '意傳',
